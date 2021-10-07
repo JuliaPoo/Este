7439f2253ffb238b4f85c080b0d9c5e0115b3f15
Julia <julia.poo.poo.poo@gmail.com>
2021-10-07 18:09:04 +0800

    Added @lilozz2 contribution: Preliminary parsing and visualizing of data
    Signed-off-by: Julia <julia.poo.poo.poo@gmail.com>

---
+++ postprocessing/main.py
@@ -0,0 +1,26 @@
+from format_helper import DataParser
+import os
+import json
+import pathlib
+
+IN_DIR = pathlib.Path(r"gen\Este-out")
+OUT_DIR = pathlib.Path(r"web\gen")
+
+def getPids() -> list:
+    files = os.listdir(IN_DIR)
+    all_prefix = set(f.split('.')[0] for f in files)
+    all_pids = [int(f[3:]) for f in all_prefix]
+    return all_pids
+
+def main():
+
+    all_pids = getPids()
+    with open(OUT_DIR / "pids.json", "w") as f:
+        json.dump(all_pids, f)
+
+    for pid in all_pids:
+        db = DataParser(IN_DIR, pid)
+        db.outputToJson(OUT_DIR / f"pid_{pid}_este.json")
+
+if __name__ == "__main__":
+    main()
